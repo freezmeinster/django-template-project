93f877326c878f1adbea128d99eb8e00f6b74919
Bramandityo Prabowo <freez_meinster@yahoo.co.id>
2012-07-18 23:28:00 +0700

    rubah setting

--- settings.py
+++ settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'django.contrib.admin',
     'livesettings',
     'keyedcache',
