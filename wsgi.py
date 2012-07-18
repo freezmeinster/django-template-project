10a42d8054dac6346c674e481aaaa18f0fae15a0
Bramandityo Prabowo <freez_meinster@yahoo.co.id>
2012-07-18 22:53:02 +0700

    rubah settings.py , database diset fullpath. & Wsgi ditambauhin default search pathnya

--- wsgi.py
+++ wsgi.py
@@ -1,19 +1,6 @@
-"""
-WSGI config for invent project.
+import os,sys
 
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
-
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
-
-"""
-import os
+sys.path.insert(0,os.path.abspath(os.path.dirname(__file__)))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
